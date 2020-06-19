c3bb925c60fa159f589ec220b1a7e692871fb932
ArielHolman <ArielARodrigues01@gmail.com>
2020-06-19 01:39:26 -0500

    updated contact mobile version.

--- contact/scripts.js
+++ contact/scripts.js
@@ -4,3 +4,12 @@ new URLSearchParams(window.location.search).forEach((value, name) => {
   placeForResults.append(`${name} : ${value}`)
   placeForResults.append(document.createElement("br"))
 })
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
